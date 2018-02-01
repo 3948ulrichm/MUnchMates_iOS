1b85cff4d63a87668ee577bddf4ac41c4ed2a273
3948ulrichm <31642489+3948ulrichm@users.noreply.github.com>
2018-02-01 01:45:37 -0600

    2.1.2018 (1) - MU

--- Classes/AddDeleteClubsOrgsViewController.swift
+++ Classes/AddDeleteClubsOrgsViewController.swift
@@ -15,7 +15,8 @@ class AddDeleteClubsOrgsViewController: UIViewController, UITableViewDelegate, U
     //MARK: Properties
     let ref = Database.database()
     let cellId = "AddDeleteClubsOrgsCell"
-    var clubsOrgs: [clubsOrgsStruct] = []
+    var clubsOrgsCLUBSORGS: [clubsOrgsStruct] = []
+    var clubsOrgsUSERS: [clubsOrgsStruct] = []
     let uid = Auth.auth().currentUser?.uid
     var selectedClubOrg = clubsOrgsStruct()
 
@@ -26,45 +27,77 @@ class AddDeleteClubsOrgsViewController: UIViewController, UITableViewDelegate, U
                 
                 var fireAccountArray: [clubsOrgsStruct] = []
                 
-                for fireAccount in snapshot.children {
-                    let fireAccount = clubsOrgsStruct(snapshot: fireAccount as! DataSnapshot)
-                    fireAccountArray.append(fireAccount)
+                for fireAccountCLUBSORGS in snapshot.children {
+                    let fireAccountCLUBSORGS = clubsOrgsStruct(snapshot: fireAccountCLUBSORGS as! DataSnapshot)
+                    fireAccountArray.append(fireAccountCLUBSORGS)
                 }
                 
-                self.clubsOrgs = fireAccountArray
+                self.clubsOrgsCLUBSORGS = fireAccountArray
 
                 self.tableView.delegate = self
                 self.tableView.dataSource = self
                 self.tableView.reloadData()
         })
         
+        
+        //load checkmarks for clubs the user has saved
+        ref.reference(withPath:"USERS/\(uid!)/clubsOrgs/").queryOrdered(byChild:"cname").observe(.value, with:
+            { snapshot in
+                
+                var fireAccountArrayUSERS: [clubsOrgsStruct] = []
+                
+                for fireAccountUSERS in snapshot.children {
+                    let fireAccountUSERS = clubsOrgsStruct(snapshot: fireAccountUSERS as! DataSnapshot)
+                    fireAccountArrayUSERS.append(fireAccountUSERS)
+                }
+                
+                self.clubsOrgsUSERS = fireAccountArrayUSERS
+                
+                self.tableView.delegate = self
+                self.tableView.dataSource = self
+                self.tableView.reloadData()
+                
+        })
+        
         super.viewDidLoad()
+
+        
     }
     
     //table view - clubsOrgs
+    //number of sections (columns??)
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
     
+    //number of rows
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return clubsOrgs.count
+        return clubsOrgsCLUBSORGS.count
     }
+
     
+    //What is in each cell?
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellId) as! AddDeleteClubsOrgsTableViewCell
-        let clubOrgInfo = self.clubsOrgs[indexPath.row]
+        let clubOrgInfo = self.clubsOrgsCLUBSORGS[indexPath.row]
         
         //Display club / org
-        cell.lblClubsOrgs?.text = clubOrgInfo.cname
-        return cell
+        var lblClubsOrgs = cell.lblClubsOrgs?.text
+        lblClubsOrgs = clubOrgInfo.cname
         
+        return cell
+
+    //DISPLAY CHECKMARKS IF USER IS IN CLUB
+    
     }
     
-    var cname:String = ""
+    var cnameCLUBSORGS:String = ""
+    var cnameUSERS:String = ""
     
-    //added by awebber to add c
+    //What happens if you select a row
+    //add checkmarks (youtu.be/5MZ-WJuSdpg)
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        cname = self.clubsOrgs[indexPath.row].cname
+
         
         if tableView.cellForRow(at: indexPath)?.accessoryType == UITableViewCellAccessoryType.checkmark {
             tableView.cellForRow(at: indexPath)?.accessoryType = UITableViewCellAccessoryType.none
