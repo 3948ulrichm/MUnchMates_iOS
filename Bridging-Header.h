69ed5105389b50bd9437fe0017992e05a3db51d4
awebber <andrew.webber@marquette.edu>
2018-02-05 21:45:31 -0600

    Messaging
    First attempt at messaging. Currently not set up to handle multiple conversations and all messages will appear in the table. Next commit will aim to add conversation ability

---
+++ Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Bridging-Header.h
+//  MUnchMates
+//
+//  Created by Andrew Webber on 1/21/18.
+//  Copyright © 2018 Michael Ulrich. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import <JSQMessagesViewController/JSQMessages.h>
+
+#endif /* Bridging_Header_h */
