264135672a78aa14c36eecbbd976c841b5bd9f3f
Michael Ulrich <michael.ulrich@marquette.edu>
2018-03-19 18:27:07 -0500

    3.19.2018 (1)

---
+++ Classes/BarButtonFont.swift
@@ -0,0 +1,24 @@
+//
+//  BarButtonFont.swift
+//  MUnchMates
+//
+//  Created by Michael Ulrich on 3/19/18.
+//  Copyright © 2018 Michael Ulrich. All rights reserved.
+//
+
+import UIKit
+
+class BarButtonFont: UIBarButtonItem {
+    override func awakeFromNib() {
+        style = .plain
+        tintColor = .MUnchMatesBlue
+        
+        //Set font name and size
+        guard let font = UIFont(name: "Arial", size: 16) else {
+            return
+        }
+        
+        setTitleTextAttributes([NSAttributedStringKey.font:font], for: .normal)
+    }
+}
+
