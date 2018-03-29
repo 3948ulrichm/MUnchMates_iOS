cedbff0cb1ed83b061d2e992c498e7a074a80120
awebber <andrew.webber@marquette.edu>
2018-03-28 20:10:00 -0500

    push notifications
    Push notification in app delegate

--- MUnchMates/AppDelegate.swift
+++ MUnchMates/AppDelegate.swift
@@ -19,6 +19,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { (success, error) in
+            
+            if error == nil {
+                print("Successful Authorization")
+            }
+        }
+        application.registerForRemoteNotifications()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(self.refreshToken(notification:)), name: NSNotification.Name.InstanceIDTokenRefresh, object: nil)
+
         return true
     }
 
@@ -30,6 +41,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        Messaging.messaging().shouldEstablishDirectChannel = false
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
@@ -38,6 +50,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        FBHandler()
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
@@ -92,6 +105,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             }
         }
     }
+    
+    @objc func refreshToken(notification: NSNotification) {
+        let refreshToken = InstanceID.instanceID().token()!
+        print("*** \(refreshToken) ***")
+        
+        FBHandler()
+    }
+    
+    func FBHandler() {
+        Messaging.messaging().shouldEstablishDirectChannel = true
+    }
+    
 
 }
 
